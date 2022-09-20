e721f6a108d98576084057cd50dd3589f1a9c898
jbresky <joaquin_bresky@hotmail.com>
2022-09-20 07:51:04 -0300

    testing-fonts

--- components/layout.js
+++ components/layout.js
@@ -6,11 +6,11 @@ export default function Layout() {
         <>
             <Head>
                 <title>N - Ecodev</title>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons+Outlined&display=optional" />
+                {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons+Outlined&display=optional" /> */}
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=optional" rel="stylesheet" />
                 {/* <link rel="stylesheet" href="/css/styles.css"/> */}
                 <link rel="icon" href="/favicon.ico" />
-                <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional" />
+                {/* <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional" /> */}
                 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
             </Head>
 
