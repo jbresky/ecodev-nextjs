9d2aff71ed8a448bd3c65d743ac1415761957955
jbresky <joaquin_bresky@hotmail.com>
2022-10-19 00:07:16 -0300

    deleting folder

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,54 @@
-import styles from '../styles/Home.module.css'
-import Layout from '../components/Layout'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import Product from '../components/Product'
-import axios from 'axios';
+// import styles from '../styles/Home.module.css'
+// import Layout from '../components/Layout'
+// import Link from 'next/link'
+// import { useEffect, useState } from 'react'
+// import Product from '../components/Product'
+// import axios from 'axios';
 
 export default function Home() {
+  return <h1>Hi</h1>
+  // const [products, setProducts] = useState([]);
+  // const [errorMessage, setErrorMessage] = useState('');
 
-  const [products, setProducts] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-
-  useEffect(() => {
-    axios
-      .get('/api/products')
-      .then(({ data }) => {
-        setProducts(data.products)
-      })
-      .catch(error => {
-        let message;
-        if (error.response) {
-          message = error.response.data.message;
-        } else {
-          message = error.message;
-        }
-        setErrorMessage(message);
-      })
-  }, [])
-  return (
-    <>
-      <Layout title='Home'>
-        <section className={styles.banner}>
-          <div className={styles.bannerContent}>
-            <Link href='/store'>
-              <a>Ver productos</a>
-            </Link>
-          </div>
-        </section>
-        <div className='items'>
-          <h1>Ofertas</h1>
-          {products && products.map(item => {
-            if (item.insale === 1) {
-              return <Product
-                showAs='home'
-                key={item.id}
-                item={item}
-              />
-            }
-          })}
-        </div>
-      </Layout>
-    </>
-  )
+  // useEffect(() => {
+  //   axios
+  //     .get('/api/products')
+  //     .then(({ data }) => {
+  //       setProducts(data.products)
+  //     })
+  //     .catch(error => {
+  //       let message;
+  //       if (error.response) {
+  //         message = error.response.data.message;
+  //       } else {
+  //         message = error.message;
+  //       }
+  //       setErrorMessage(message);
+  //     })
+  // }, [])
+  // return (
+  //   <>
+  //     <Layout title='Home'>
+  //       <section className={styles.banner}>
+  //         <div className={styles.bannerContent}>
+  //           <Link href='/store'>
+  //             <a>Ver productos</a>
+  //           </Link>
+  //         </div>
+  //       </section>
+  //       <div className='items'>
+  //         <h1>Ofertas</h1>
+  //         {products && products.map(item => {
+  //           if (item.insale === 1) {
+  //             return <Product
+  //               showAs='home'
+  //               key={item.id}
+  //               item={item}
+  //             />
+  //           }
+  //         })}
+  //       </div>
+  //     </Layout>
+  //   </>
+  // )
 }
--- pages/users/login.js
+++ pages/users/login.js
@@ -1,55 +1,56 @@
-import Link from "next/link";
-import Layout from "../../Components/Layout";
-import SmallFooter from "../../Components/SmallFooter";
-import style from '../../styles/login.module.css'
+// import Link from "next/link";
+// import Layout from "../../Components/Layout";
+// import SmallFooter from "../../Components/SmallFooter";
+// import style from '../../styles/login.module.css'
 
 export default function Login(){
     return (
-        <>
-        <Layout/>
-        <div className="formHeader">
-        <h3 className={style.titleForm}>Inciar sesión</h3>
-        <h4 className={style.community}>¿Aún no tienes una cuenta?
-        <Link href="/users/register">
-        <a style={{fontWeight: 700}}> Unite a la comunidad</a>
-        </Link>
-        </h4>
-      </div>
+      <h1>Hi</h1>
+      //   <>
+      //   <Layout/>
+      //   <div className="formHeader">
+      //   <h3 className={style.titleForm}>Inciar sesión</h3>
+      //   <h4 className={style.community}>¿Aún no tienes una cuenta?
+      //   <Link href="/users/register">
+      //   <a style={{fontWeight: 700}}> Unite a la comunidad</a>
+      //   </Link>
+      //   </h4>
+      // </div>
 
-      <main class={style.loginForm}>
-            <form action="/users/login" method="POST">
-              <div class={style.inputForm}>
-              <input type="email" name="email" placeholder="Email" id="inputEmail"/>
-              <span class="valid-register" id="email-login"></span>
-            </div>
+      // <main class={style.loginForm}>
+      //       <form action="/users/login" method="POST">
+      //         <div class={style.inputForm}>
+      //         <input type="email" name="email" placeholder="Email" id="inputEmail"/>
+      //         <span class="valid-register" id="email-login"></span>
+      //       </div>
 
-            <div class={style.inputForm}>
-              <input type="password" name="password" placeholder="Contraseña" id="inputPassword"/>
-              <span class="valid-register" id="password-login"></span>
-            </div>
+      //       <div class={style.inputForm}>
+      //         <input type="password" name="password" placeholder="Contraseña" id="inputPassword"/>
+      //         <span class="valid-register" id="password-login"></span>
+      //       </div>
 
-            <div class={style.formCookie}>
-              <div>
-              <label class="remb" htmlFor="remb">Recordar</label>
-              <input type="checkbox" id={style.remb} name="remember"/>
-            </div>
-              <label>
-                <Link href='#'>
-                  <a>¿Has olvidado tu contraseña?</a>
-                  </Link>
-                  </label>
+      //       <div class={style.formCookie}>
+      //         <div>
+      //         <label class="remb" htmlFor="remb">Recordar</label>
+      //         <input type="checkbox" id={style.remb} name="remember"/>
+      //       </div>
+      //         <label>
+      //           <Link href='#'>
+      //             <a>¿Has olvidado tu contraseña?</a>
+      //             </Link>
+      //             </label>
                   
-            </div>
-            <br/>
-            <div class={style.formBtnSend}>
-              <button type="submit">Ingresar</button>
-            </div>
-            </form>
+      //       </div>
+      //       <br/>
+      //       <div class={style.formBtnSend}>
+      //         <button type="submit">Ingresar</button>
+      //       </div>
+      //       </form>
            
-          </main>
-        <section className={style.preFooter}></section>
-        <SmallFooter/>
-       </> 
+      //     </main>
+      //   <section className={style.preFooter}></section>
+      //   <SmallFooter/>
+      //  </> 
       
     )
 }
