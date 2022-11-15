1b8ab123039d0deffa2358c5e6e15ddc19b3f82c
jbresky <joaquin_bresky@hotmail.com>
2022-11-15 17:26:04 -0300

    database config

--- database/config/config.mjs
+++ database/config/config.mjs
@@ -24,7 +24,7 @@ export default {
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      host: process.env.DB_HOSTNAME,
+      host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       logging: false,
       dialect: 'mysql'
